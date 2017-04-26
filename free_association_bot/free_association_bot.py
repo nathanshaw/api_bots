300ccbdcac926b280c0d5801ce57cbe85ce47ea8
nathanshaw <nathanshawsemail@gmail.com>
2017-04-25 17:32:21 -0700

    repressed warnings, got the print outs working better

--- free_association_bot/free_association_bot.py
+++ free_association_bot/free_association_bot.py
@@ -4,11 +4,13 @@ import wikipedia
 import string
 import random
 import time
+import warnings
 
 number = 1
 voices = ["Princess",
           "Fred"]
-stop_chars = ["-", '"', "'", "/n", "[", "]", "{", "}", "(", ")", ";", ":"]
+stop_chars_comma = ["-", "/n", "[", "]", "{", "}", "(", ")", ";", ":"]
+stop_chars_space= ['"', "'"]
 allowed_chars = [
 'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I',
 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R',
@@ -39,30 +41,38 @@ def getRawSummary(term):
         return raw_summary
     return seed_term
 
+def processWikiText(raw_summary):
+    wiki_summary = ''
+    for char in raw_summary:
+        if char in allowed_chars:
+            wiki_summary += char
+        if char in stop_chars_comma:
+            wiki_summary += ", "
+        if char in stop_chars_space:
+            wiki_summary += " "
+    wiki_summary = wiki_summary.replace(", .", ".")
+    wiki_summary = wiki_summary.replace("  ", " ")
+    wiki_summary = wiki_summary.replace(" .", ".")
+    wiki_summary = wiki_summary.replace(" ,", ",")
+    return wiki_summary
+
 def getSummary(term, voice="Bruce"):
     """
     TODO
     """
     search = wikipedia.search(term, 'html.parser')
     raw_summary = getRawSummary(term)
-
-    wiki_summary = ''
-    for char in raw_summary:
-        if char in allowed_chars:
-            wiki_summary += char
-        if char in stop_chars:
-            wiki_summary += ", "
-    wiki_summary.replace("\n", ", ")
+    wiki_summary = processWikiText(raw_summary)
 
     # time to actually say some things
     if platform == "darwin":
         command = "say -v " + voice + " " + wiki_summary
-        print(voice, ': "', wiki_summary, '"')
+        print(wiki_summary)
     else:
         command = '"' + wiki_summary + '"' + "| espeak"
         print(wiki_summary)
     os.system(command)
-    print("--------------------------------------------")
+    print(" ")
 
     last_word = ''.join(
             [c for c in raw_summary.split()[-1] if c.isalpha()])
@@ -84,5 +94,6 @@ else:
     seed_term = "love"
     personality = voices[random.randrange(len(voices))]
 
-while(True):
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
     getSummary(seed_term, personality)
